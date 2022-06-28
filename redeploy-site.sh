d2df665fe82bd905a343aa2267bab3b10f2f294c
Catherine Laserna <49669196+cjlaserna@users.noreply.github.com>
2022-06-27 23:27:16 -0700

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-command tmux kill-server
-PID=$(netstat -nlp | grep 0.0.0.0:5000 | awk '{print $NF}' | awk 'BEGIN { FS="/" } {print $1}')
-if [[ $PID =~ ^[0-9]{0,}$ ]];
-then
-        command kill $PID
-fi
 command cd flask-blog-personal
 command git fetch && git reset origin/main --hard
 poetry install
-command tmux new-session -d -s siteDeploy
-command tmux send-keys -t siteDeploy.0 'cd flask-blog-personal; export DATOCMS_READONLY_TOKEN=SECRET FLASKNV=production FLASK_DEBUG=off; poetry run flask run --host=0.0.0.0' C-m
+command systemctl daemon-reload
+command systemctl restart myportfolio
